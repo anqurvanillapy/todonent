cde446cb260552f630ed8912f381bc755255cd1a
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-05-29 03:05:25 +0800

    FIXME: not a good way for todo-app global change; but everything works now

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ class TodoApp extends HTMLElement {
   constructor () {
     super()
     this._nitem = 0
+    this._done = 0
     this.attachShadow({mode: 'open'})
   }
 
-  static get observedAttributes () { return ['nitem'] }
+  static get observedAttributes () { return ['nitem', 'done'] }
   attributeChangedCallback (attr, oldValue, newValue) {
     if (oldValue !== newValue) this[attr] = newValue
   }
@@ -21,7 +22,16 @@ class TodoApp extends HTMLElement {
     if (value !== this._nitem) {
       this._nitem = parseInt(value)
       this.setAttribute('nitem', value)
-      // this.display()
+      this.updateStatusbar()
+    }
+  }
+
+  get done () { return this._done }
+  set done (value) {
+    if (value !== this._done) {
+      this._done = parseInt(value)
+      this.setAttribute('done', value)
+      this.updateStatusbar()
     }
   }
 
@@ -30,50 +40,134 @@ class TodoApp extends HTMLElement {
   render () {
     let todoBox = wrapFn`
       <style>
-      * {
-        box-sizing: border-box;
-      }
-
       header h1 {
         color: #ccc;
         text-align: center;
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
       }
 
-      .todobox {
+      #todobox {
         padding: .5em;
         border: solid 1px #ccc;
-        box-shadow: 3px 3px #999;
+        border-radius: 3px;
+        box-shadow: 0px 4px 5px #999;
+        display: flex;
+        justify-content: space-around;
       }
-        .todobox > * { font-size: 1.5em; }
-        .todobox > input { width: 90%; }
+        #todobox > * { font-size: 1.5em; }
+        #todobox > input { width: 90%; }
       </style>
       <header><h1>Todos</h1></header>
-      <div class="todobox">
+      <div id="todobox">
         <input id="entry" type="text">
         <button id="add" type="button">+</button>
-      </div>`
+      </div>
+      <p id="statusbar"></p>`
+
+    // Prepare for data-binding.
+    this.inputEntry = todoBox.getElementById('entry')
+    this.btnAdd = todoBox.getElementById('add')
+    this.statusbar = todoBox.getElementById('statusbar')
 
     // Listeners.
-    let entry = todoBox.getElementById('entry')
-    todoBox.getElementById('add').addEventListener('click', _ => {
-      if (!entry.value) return
-      console.log(entry.value)
+    this.btnAdd.addEventListener('click', _ => {
+      if (!this.inputEntry.value) return
+
+      let newItem = document.createElement('todo-item')
+      newItem.setAttribute('val', this.inputEntry.value)
+
+      ++this.nitem
+      this.inputEntry.value = ''
+
+      document.body.appendChild(newItem)
     })
 
     this.shadowRoot.appendChild(todoBox)
+  }
+
+  updateStatusbar () {
+    this.statusbar.textContent = `${this.done} / ${this.nitem} done.`
   }
 }
 
 class TodoItem extends HTMLElement {
   constructor () {
     super()
+    this._val = ''
     this.attachShadow({mode: 'open'})
+  }
+
+  static get observedAttributes () { return ['val'] }
+  attributeChangedCallback (attr, oldValue, newValue) {
+    if (oldValue !== newValue) this[attr] = newValue
+  }
+
+  get val () { return this._val }
+  set val (value) {
+    if (value !== this._val) {
+      this._val = value
+      this.setAttribute('val', value)
+      // Setting attributes might precede rendering.
+      if (this.labelVal) this.updateDone()
+    }
   }
 
   connectedCallback () { this.render() }
 
-  render () { }
+  render () {
+    let todoItem = wrapFn`
+      <style>
+      .container {
+        margin: 1em auto;
+        padding: 1em;
+        box-shadow: 0px 4px 5px #999;
+      }
+      </style>
+      <div class="container">
+        <button id="remove" type="button">&times;</button>
+        <input id="done" type="checkbox">
+        <label id="val" for="done"></label>
+      </div>`
+
+    this.btnRemove = todoItem.getElementById('remove')
+    this.checkboxDone = todoItem.getElementById('done')
+    this.labelVal = todoItem.getElementById('val')
+    this.todoBox = document.querySelector('todo-app')
+
+    this.checkboxDone.addEventListener('click', _ => {
+      this.updateDone(this.checkboxDone.checked)
+    })
+
+    this.btnRemove.addEventListener('click', _ => {
+      this.updateRemove(this.checkboxDone.checked)
+    })
+
+    this.labelVal.textContent = `${this.val}`
+    this.shadowRoot.appendChild(todoItem)
+  }
+
+  updateDone (isDone) {
+    // Update label.
+    this.labelVal.style.textDecoration = isDone ? 'line-through' : 'none'
+
+    // Update todo-app's statusbar.
+    // FIXME: Not visible enough for a global change (todo-app).
+    let done = parseInt(this.todoBox.done) || 0
+
+    if (done) done = (isDone) ? ++done : --done
+    if (!done && isDone) ++done
+    this.todoBox.setAttribute('done', done)
+  }
+
+  updateRemove (isDone) {
+    let nitem = parseInt(this.todoBox.nitem) || 0
+    let done = parseInt(this.todoBox.done) || 0
+
+    if (isDone) --done
+    this.todoBox.setAttribute('nitem', --nitem)
+    this.todoBox.setAttribute('done', done)
+
+    document.body.removeChild(this)
+  }
 }
 
 customElements.define('todo-app', TodoApp)
