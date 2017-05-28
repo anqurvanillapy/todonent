7b4a3b8151c0e76de836d272253148f71c597f94
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-05-28 23:41:52 +0800

    updated js: wrapFn

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let wrapFn = text => {
+  let tmpl = document.createElement('template')
+  tmpl.innerHTML = text
+  return document.importNode(tmpl.content, true)
+}
+
 class TodoApp extends HTMLElement {
   constructor () {
     super()
@@ -22,27 +28,40 @@ class TodoApp extends HTMLElement {
   connectedCallback () { this.render() }
 
   render () {
-    let todoBox = document.createElement('template')
-    todoBox.innerHTML =
-    `<style>
+    let todoBox = wrapFn`
+      <style>
       * {
-        margin: 0;
-        padding: 0;
         box-sizing: border-box;
       }
 
-      .container {
-        padding: 2px;
-        border: solid 1px #999;
-        border-radius: 2px;
+      header h1 {
+        color: #ccc;
+        text-align: center;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
       }
+
+      .todobox {
+        padding: .5em;
+        border: solid 1px #ccc;
+        box-shadow: 3px 3px #999;
+      }
+        .todobox > * { font-size: 1.5em; }
+        .todobox > input { width: 90%; }
       </style>
-      <div class="container">
-        <input type="text" name="entry">
-        <button type="button">Add</button>
+      <header><h1>Todos</h1></header>
+      <div class="todobox">
+        <input id="entry" type="text">
+        <button id="add" type="button">+</button>
       </div>`
 
-    this.shadowRoot.appendChild(document.importNode(todoBox.content, true))
+    // Listeners.
+    let entry = todoBox.getElementById('entry')
+    todoBox.getElementById('add').addEventListener('click', _ => {
+      if (!entry.value) return
+      console.log(entry.value)
+    })
+
+    this.shadowRoot.appendChild(todoBox)
   }
 }
 
@@ -51,6 +70,11 @@ class TodoItem extends HTMLElement {
     super()
     this.attachShadow({mode: 'open'})
   }
+
+  connectedCallback () { this.render() }
+
+  render () { }
 }
 
 customElements.define('todo-app', TodoApp)
+customElements.define('todo-item', TodoItem)
